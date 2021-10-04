4e97f9df26a74837af20649337ab4229ed31f1cf
“[Зорин <[evgenijzorin@bk.ru]>
2021-10-04 10:52:45 +0300

    create Task for lesson 1

---
+++ Lesson_1/Task_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static string chekingNumber_SimpleComplex(int number)
+        {
+            int d = 0; // числовой индикатор, соответствующий количеству чисел на которые можно разделить число без остатка
+            int i = 2; // первое присвоение значения делителю. 
+            while(i < number)
+            {
+                if(number % i == 0)
+                {
+                    d++;
+                }
+                i++; // инкремент делителя
+            }
+            string ansver;
+            if (d == 0)
+            {
+                ansver = $"Число {number} - простое.";                
+            }
+            else
+            {
+                ansver =$"Число {number} - составное.";
+            }
+
+
+            return ansver;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(chekingNumber_SimpleComplex(1223));
+            Console.WriteLine(chekingNumber_SimpleComplex(123));
+            Console.WriteLine(chekingNumber_SimpleComplex(12));
+            Console.WriteLine(chekingNumber_SimpleComplex(13));
+            Console.WriteLine(chekingNumber_SimpleComplex(43));
+            Console.WriteLine(chekingNumber_SimpleComplex(2));
+        }
+    }
+}
---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // O(N)
+                    { 
+                        int y = 0; // O(1)
+                        if (j != 0) { y = k / j; }  // O(1)
+                        sum += inputArray[i] // O(1)
+                            + i // O(1)
+                            + k // O(1)
+                            + j // O(1)
+                            + y; // O(1)
+                        // Асимптотическая сложность Функции StrangeSum = О(7N * N * N)
+                        // Сокращаем постоянные множители = О(N^3) 
+                    }
+                }
+            }
+            return sum;
+        }                
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Асимптотическая сложность Функции StrangeSum = О(N^3)");
+        }
+    }
+}
